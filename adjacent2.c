9b52440cb69ecf7ff1db45e6ba9f6ab39b835cfc
StevenGamer177 <165827697+StevenGamer177@users.noreply.github.com>
2024-10-02 17:20:49 +0000

    terminated

--- adjacent2.c
+++ adjacent2.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char hi[] = {'H', 'i', ' ', 'a', 'l', 'l', '!', '!' };
+int main() { // these char arrays do not have a 0 at the end. 
+             // C will ONLY implicityly insert a 0 byte at the end of string literals
+             //the hi array and the helloeveryone array are stored in adjacent
+             //memory locations, and neither is null-terminated (neither ends in a 0 byte)
+    char hi[] = {'H', 'i', ' ', 'a', 'l', 'l', '!', '!', 0 };
     char helloeveryone[] = { 'H', 'e', 'l', 'l', 'o', ',', ' ',
-                             'e', 'v', 'e', 'r', 'y', 'o', 'n', 'e', '!' };
+                             'e', 'v', 'e', 'r', 'y', 'o', 'n', 'e', '!', 0 };
     puts(hi);
     puts(helloeveryone);
     printf("%ld\n", strlen(hi));
